b918a708442ae9b063e07010a6105b685fc55457
Damon Janis <damonvjanis@gmail.com>
2019-08-18 14:55:28 -0600

    Add config for publishing to hex.pm

--- mix.exs
+++ mix.exs
@@ -5,7 +5,10 @@ defmodule Telnyx.MixProject do
     [
       app: :telnyx,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.6",
+      description: "Telnyx API Elixir Client",
+      source_url: "https://github.com/damonvjanis/telnyx",
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -17,10 +20,19 @@ defmodule Telnyx.MixProject do
     ]
   end
 
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["damon janis <damonvjanis@gmail.com>"],
+      links: %{github: "https://github.com/damonvjanis/telnyx"}
+    ]
+  end
+
   defp deps do
     [
       {:mojito, "~> 0.4.0"},
-      {:jason, "~> 1.1"}
+      {:jason, "~> 1.1"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
     ]
   end
 end
