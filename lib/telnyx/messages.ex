c2c13819f5aad3b0de908f0fcf39b6a22cf7a3d7
Damon Janis <damon.janis@podium.com>
2019-10-01 06:40:42 -0600

    Add support for sending from alphanumeric

--- lib/telnyx/messages.ex
+++ lib/telnyx/messages.ex
@@ -69,6 +69,67 @@ defmodule Telnyx.Messages do
   end
 
   @doc """
+  Sends a message.
+
+  ## Examples
+
+    ```elixir
+    api_key = "YOUR_API_KEY"
+
+    Telnyx.Messages.create_alphanumeric(api_key, %{
+      messaging_profile_id: "3fa85f55-5717-4562-b3fc-2c963f63vga6",
+      from: "TelnyxID", # Your Telnyx alphanumeric ID
+      to: "+18445552367",
+      text: "Hello, World!"
+    })
+    ```
+    Example response:
+    ```
+    {:ok,
+      %{
+        "carrier" => "Verizon",
+        "cost" => %{"amount" => "1.23", "currency" => "USD"},
+        "created_at" => "2019-01-23T18:10:02.574Z",
+        "direction" => "outbound",
+        "errors" => [],
+        "from" => "TelnyxID",
+        "id" => "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "line_type" => "Wireless",
+        "parts" => 1,
+        "record_type" => "message",
+        "text" => "Hello world",
+        "to" => [
+          %{
+            "address" => "+18665550001",
+            "status" => "queued",
+            "updated_at" => "2019-01-23T18:10:02.574Z"
+          }
+        ],
+        "type" => "sms",
+        "updated_at" => "2019-01-23T18:10:02.574Z",
+        "use_profile_webhooks" => false,
+        "valid_until" => "2019-01-23T18:10:02.574Z",
+        "webhook_failover_url" => "https://www.example.com/callbacks",
+        "webhook_url" => "https://www.example.com/hooks"
+      }
+    }
+    ```
+
+  See https://developers.telnyx.com/docs/api/v2/messaging/Messages for more options
+  """
+  @spec create_alphanumeric(String.t(), map) :: {:ok, map} | {:error, Telnyx.error()}
+  def create_alphanumeric(api_key, params = %{messaging_profile_id: _, from: _, to: _, text: _}) do
+    Client.post(api_key, params, @base_url <> "v2/messages/alphanumeric_sender_id")
+  end
+
+  def create_alphanumeric(_api_key, params) do
+    required_params = [:messaging_profile_id, :from, :to, :text]
+    missing_key = Enum.find(required_params, fn key -> params[key] == nil end)
+
+    {:error, %Telnyx.Error{message: ~s(required param "#{missing_key}" not given)}}
+  end
+
+  @doc """
   Retrieves a message.
 
   ## Examples
--- test/telnyx/message_test.exs
+++ test/telnyx/message_test.exs
@@ -16,15 +16,11 @@ defmodule Telnyx.MessagesTest do
 
   describe "create/2" do
     test "fails without required params" do
-      {:error, failed_result} = Telnyx.Messages.create(@test_key, @invalid_params)
-
-      assert failed_result.__struct__ == Telnyx.Error
+      assert match?({:error, %Telnyx.Error{}}, Telnyx.Messages.create(@test_key, @invalid_params))
     end
 
     test "succeeds with valid params" do
-      {:ok, successful_result} = Telnyx.Messages.create(@test_key, @valid_params)
-
-      assert successful_result["text"] == "Hello world"
+      assert match?({:ok, %{"text" => _}}, Telnyx.Messages.create(@test_key, @valid_params))
     end
   end
 
@@ -36,6 +32,24 @@ defmodule Telnyx.MessagesTest do
 
     test "succeeds with valid uid" do
       assert match?({:ok, _}, Telnyx.Messages.retrieve(@test_key, @uuid))
+    end
+  end
+
+  describe "create_alphanumeric/2" do
+    test "fails without required params" do
+      assert match?(
+               {:error, %Telnyx.Error{}},
+               Telnyx.Messages.create_alphanumeric(@test_key, @invalid_params)
+             )
+    end
+
+    test "succeeds with valid params" do
+      valid_alphanumeric = Map.put(@valid_params, :from, "TelnyxID")
+
+      assert match?(
+               {:ok, %{"text" => _}},
+               Telnyx.Messages.create_alphanumeric(@test_key, valid_alphanumeric)
+             )
     end
   end
 end
